7bfb073f8cb95706ea0c94c6a530d9ed8d748a5f
Oscar <oscar.bertaud-velten@etu.univ-lyon1.fr>
2025-03-17 17:07:16 +0100

    player api returns json

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request
+import json
+
 from LOL_Pro_API import players_api
+from LOL_Pro_API import teams_api
 
 app = Flask(__name__)
 
 @app.route('/player')
 def get_player_info():
     pseudo = request.args.get('name', '')
-    return players_api.Player(pseudo).__str__()
+    return json.dumps(players_api.Player(pseudo))
+
+@app.route('/team')
+def get_team_info():
+    pseudo = request.args.get('name', '')
+    return teams_api.Team(pseudo).__str__()
 
 @app.route('/')
 def helloworld():
     return 'hello world'
 
-app.run("localhost", 3030)
+app.run("0.0.0.0", 3000)
